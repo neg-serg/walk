84e153ceaf1c8bac7188445f8eb41a8c382a460a
Sergey Miroshnichenko <serg.zorg@gmail.com>
2025-08-31 08:21:15 +0300

    nix: add flake for Go build and devShell

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "walk - TUI file navigator (Go)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        lib = pkgs.lib;
+        version = if self ? rev then self.rev else "dev";
+      in {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "walk";
+            inherit version;
+            src = ./.;
+            vendorHash = "sha256-a66vA6eFzckxBpVtHaX0PBtulTBPbh7c6HY3dIZAym8=";
+            # Build the main package in the repo root.
+            subPackages = [ "." ];
+          };
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/walk";
+        };
+
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.go
+            pkgs.gopls
+            pkgs.delve
+          ];
+        };
+
+        checks.build = self.packages.${system}.default;
+      }
+    );
+}
